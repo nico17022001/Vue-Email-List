9e15876a096be0c4a326c4edaa030b76734beda3
nico17022001 <nico17022001boolean@gmail.com>
2023-04-26 17:39:21 +0200

    fine-esercizio

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const {createApp} = Vue 
+const {createApp} = Vue ;
 
 createApp({
-  document(){
+  data(){
     return{
-      
+      title:"Email List",
+      apiUrl:"https://flynn.boolean.careers/exercises/api/random/mail",
+      emails:[],
+      limit:10
     }
-  }
-})
+  },
+
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then(res => {
+        this.emails.push(res.data.response);
+        console.log(this.emails);
+      })
+    }
+  },
+
+  mounted(){
+    for(let i = 0; i < this.limit; i++){
+      this.getApi()
+    }
+  },
+
+}).mount("#app")
